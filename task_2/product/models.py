68977f95f1ea3eafb77d38b3546fb69f5fd0db09
D-Delson <delson150203@gmail.com>
2024-04-01 13:25:04 +0530

    updated model

--- task_2/product/models.py
+++ task_2/product/models.py
@@ -1,26 +1,30 @@
 import uuid
 from django.db import models
 
-class SubCategory(models.Model):
+class BaseModel(models.Model):
+    MAX_LENGTH = 255
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField("subcategory_name", max_length=255)
+
+    class Meta:
+        abstract = True
+
+class SubCategory(BaseModel):
+    name = models.CharField("subcategory_name", max_length = BaseModel.MAX_LENGTH)
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-class Category(models.Model):
-    uuid = models.UUIDField(default = uuid.uuid4, editable=False)
-    name = models.CharField("category_name", max_length=255)
+class Category(BaseModel):
+    name = models.CharField("category_name", max_length=BaseModel.MAX_LENGTH)
     subcategory = models.ForeignKey('SubCategory', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
     
 class Product(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField("product_name", max_length=255)
+    name = models.CharField("product_name", max_length=BaseModel.MAX_LENGTH)
     price = models.DecimalField(max_digits = 10, decimal_places=2)
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
     subcategory = models.ForeignKey('SubCategory', on_delete=models.PROTECT)

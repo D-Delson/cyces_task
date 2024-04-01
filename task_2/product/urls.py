dc442f57ffade6836e970a07a672e995cedb9c21
D-Delson <delson150203@gmail.com>
2024-04-01 16:58:54 +0530

    add class based view

--- task_2/product/urls.py
+++ task_2/product/urls.py
@@ -2,18 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('products_add/', views.product_add, name='product_add'),
-    path('products_update/<int:i>', views.product_update, name='product_update'),
-    path('product_delete/<int:i>', views.product_delete, name='product_delete'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('products_add/', views.ProductCreateView.as_view(), name='product_add'),
+    path('products/products_update/<int:pk>/update', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product_delete/<int:pk>', views.ProductDeleteView.as_view(), name='product_delete'),
 
-    path('category/', views.category_list, name='category_list'),
-    path('category_add/', views.category_add, name='category_add'),
-    path('category_update/<int:i>', views.category_update, name='category_update'),
-    path('category_delete/<int:i>', views.category_delete, name='category_delete'),
+    path('category/', views.CategoryListView.as_view(), name='category_list'),
+    path('category_add/', views.CategoryCreateView.as_view(), name='category_add'),
+    path('category_update/<int:pk>', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('category_delete/<int:pk>', views.CategoryDeleteView.as_view(), name='category_delete'),
 
-    path('subcategory/', views.subcategory_list, name='subcategory_list'),
-    path('subcategory_add/', views.subcategory_add, name='subcategory_add'),
-    path('subcategory_update/<int:i>', views.subcategory_update, name='subcategory_update'),
-    path('subcategory_delete/<int:i>', views.subcategory_delete, name='subcategory_delete')
+    path('subcategory/', views.SubCategoryListView.as_view(), name='subcategory_list'),
+    path('subcategory_add/', views.SubCategoryCreateView.as_view(), name='subcategory_add'),
+    path('subcategory_update/<int:pk>', views.SubCategoryUpdateView.as_view(), name='subcategory_update'),
+    path('subcategory_delete/<int:pk>', views.SubCategoryDeleteView.as_view(), name='subcategory_delete')
 ]
--- task_2/product/views.py
+++ task_2/product/views.py
@@ -1,97 +1,166 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 from .models import Product,Category, SubCategory 
-from .forms import ProductForm, CategoryForm, SubCategoryForm
 
-def product_list(request):
-    form = Product.objects.all()
-    context = {'form': form}
-    return render(request, 'product.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product.html'
 
-def product_add(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        form.save()
-        return redirect('product_list')
-    else:
-        form = ProductForm()
-        context = {'form': form}
-    return render(request, 'product_add.html', context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'price', 'category', 'subcategory']
+    template_name = 'product_add.html'
+    success_url = reverse_lazy('product_list')
 
-def product_update(request, i):
-    u = Product.objects.get(pk = i)
-    if request.method == 'POST':
-        new_data = ProductForm(request.POST, instance=u)
-        new_data.save()
-        return redirect('product_list')
-    else: 
-        form = ProductForm(instance=u)
-        context = {'form':form}
-    return render(request, 'product_update.html', context)
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'price', 'category', 'subcategory']
+    template_name = 'product_update.html'
+    success_url = reverse_lazy('product_list')
 
-def product_delete(request,i):
-    o = Product.objects.get(pk = i)
-    o.delete()
-    return redirect('product_list')
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('product_list')
 
-def category_list(request):
-    c_list = Category.objects.all()
-    context = {'category': c_list}
-    return render(request, 'category.html', context)
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
 
-def category_add(request):
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        form.save()
-        return redirect('category_list')
-    else:
-        form = CategoryForm()
-        context = {'form':form}
-    return render(request, 'category_add.html', context=context)
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category.html'
 
-def category_update(request, i):
-    u = Category.objects.get(pk=i)
-    if request.method == 'POST':
-        new_data = CategoryForm(request.POST, instance=u)
-        new_data.save()
-        return redirect('category_list')
-    else:
-        form = CategoryForm(instance=u)
-        context = {'form': form}
-    return render(request, 'category_update.html', context)
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['name', 'subcategory']
+    template_name = 'category_add.html'
+    success_url = reverse_lazy('category_list')
+ 
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['name', 'subcategory']
+    template_name = 'category_update.html'
+    success_url = reverse_lazy('category_list')
 
-def category_delete(request, i):
-    o = Category.objects.get(pk= i)
-    o.delete()
-    return redirect('category_list')
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('category_list')
 
-def subcategory_list(request):
-    s_list = SubCategory.objects.all()
-    context = {'s_list':s_list}
-    return render(request, 'subcategory.html', context=context)
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+    
+class SubCategoryListView(ListView):
+    model = SubCategory
+    template_name = 'subcategory.html'
 
-def subcategory_add(request):
-    if request.method == 'POST':
-        form = SubCategoryForm(request.POST)
-        form.save()
-        return redirect('subcategory_list')
-    else:
-        form = SubCategoryForm()
-        context = {'form':form}
-    return render(request, 'subcategory_add.html', context)
+class SubCategoryCreateView(CreateView):
+    model = SubCategory
+    fields = ['name']
+    template_name = 'subcategory_add.html'
+    success_url = reverse_lazy('subcategory_list')
 
-def subcategory_update(request, i):
-    u = SubCategory.objects.get(pk=i)
-    if request.method == "POST":
-        new_data = SubCategoryForm(request.POST, instance=u)
-        new_data.save()
-        return redirect('subcategory_list')
-    else:
-        form = SubCategoryForm(instance=u)
-        context = {'form':form}
-    return render(request, 'subcategory_update.html', context)
+class SubCategoryUpdateView(UpdateView):
+    model = SubCategory
+    fields = ['name']
+    template_name = 'subcategory_update.html'
+    success_url = reverse_lazy('subcategory_list')
 
-def subcategory_delete(request, i):
-    o = SubCategory.objects.get(pk=i)
-    o.delete()
-    return redirect('subcategory_list')
+class SubCategoryDeleteView(DeleteView):
+    model = SubCategory
+    success_url = reverse_lazy('subcategory_list')
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
+# def product_list(request):
+#     form = Product.objects.all()
+#     context = {'form': form}
+#     return render(request, 'product.html', context)
+    
+# def product_add(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)
+#         form.save()
+#         return redirect('product_list')
+#     else:
+#         form = ProductForm()
+#         context = {'form': form}
+#     return render(request, 'product_add.html', context)
+    
+# def product_update(request, i):
+#     u = Product.objects.get(pk = i)
+#     if request.method == 'POST':
+#         new_data = ProductForm(request.POST, instance=u)
+#         new_data.save()
+#         return redirect('product_list')
+#     else: 
+#         form = ProductForm(instance=u)
+#         context = {'form':form}
+#     return render(request, 'product_update.html', context)
+
+# def product_delete(request,i):
+#     o = Product.objects.get(pk = i)
+#     o.delete()
+#     return redirect('product_list')
+
+# def category_list(request):
+#     c_list = Category.objects.all()
+#     context = {'category': c_list}
+#     return render(request, 'category.html', context)
+    
+# def category_add(request):
+#     if request.method == 'POST':
+#         form = CategoryForm(request.POST)
+#         form.save()
+#         return redirect('category_list')
+#     else:
+#         form = CategoryForm()
+#         context = {'form':form}
+#     return render(request, 'category_add.html', context=context)
+
+# def category_update(request, i):
+#     u = Category.objects.get(pk=i)
+#     if request.method == 'POST':
+#         new_data = CategoryForm(request.POST, instance=u)
+#         new_data.save()
+#         return redirect('category_list')
+#     else:
+#         form = CategoryForm(instance=u)
+#         context = {'form': form}
+#     return render(request, 'category_update.html', context)
+
+# def category_delete(request, i):
+#     o = Category.objects.get(pk= i)
+#     o.delete()
+#     return redirect('category_list')
+
+# def subcategory_list(request):
+#     s_list = SubCategory.objects.all()
+#     context = {'s_list':s_list}
+#     return render(request, 'subcategory.html', context=context)
+
+# def subcategory_add(request):
+#     if request.method == 'POST':
+#         form = SubCategoryForm(request.POST)
+#         form.save()
+#         return redirect('subcategory_list')
+#     else:
+#         form = SubCategoryForm()
+#         context = {'form':form}
+#     return render(request, 'subcategory_add.html', context)
+
+# def subcategory_update(request, i):
+#     u = SubCategory.objects.get(pk=i)
+#     if request.method == "POST":
+#         new_data = SubCategoryForm(request.POST, instance=u)
+#         new_data.save()
+#         return redirect('subcategory_list')
+#     else:
+#         form = SubCategoryForm(instance=u)
+#         context = {'form':form}
+#     return render(request, 'subcategory_update.html', context)
+    
+# def subcategory_delete(request, i):
+#     o = SubCategory.objects.get(pk=i)
+#     o.delete()
+#     return redirect('subcategory_list')

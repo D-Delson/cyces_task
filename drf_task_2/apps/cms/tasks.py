5eba69dfc69b2b4e3d8b45675a358df0430bda70
D-Delson <delson150203@gmail.com>
2024-04-29 11:21:51 +0530

    updated

--- drf_task_2/apps/cms/tasks.py
+++ drf_task_2/apps/cms/tasks.py
@@ -1,34 +1,35 @@
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.utils.html import escape
+
 import os
-import json
 from celery import shared_task
 from config import settings
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from xhtml2pdf import pisa
+
+def create_pdf_from_html(html_content, file_path):
+    with open(file_path, 'wb') as pdf_file:
+        pisa.CreatePDF(html_content, dest=pdf_file)
 
 #@shared_task()
 def process_retrieve_action(user_id):
 
     from apps.common.models import User 
     from apps.web.serializers import UserReadSerializer
+
     user = User.objects.get(pk=user_id)
-    serialized_user_data = UserReadSerializer(user).data
-    
+    serializer = UserReadSerializer(instance=user)
+    user_data = serializer.data
+
+
+    template = get_template('userpdf.html')
+    html_content = template.render({'user': user_data})
+
+
     file_path = os.path.join(settings.BASE_DIR, 'saved_files', f'user_{user_id}.pdf')
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    c = canvas.Canvas(file_path, pagesize=letter)
 
-    json_data = json.dumps(serialized_user_data, indent=4)  # Convert JSON data to formatted string
-    lines = json_data.split('\n')
-    y_offset = 730  # Initial y-coordinate
-    for line in lines:
-        c.drawString(100, y_offset, line)
-        y_offset -= 10
-    c.showPage()
-    c.save()
+
+    create_pdf_from_html(html_content, file_path)
 
     return file_path
-
-    
-    # with open(file_path, 'w') as file:
-    #     json.dump(serialized_user_data, file)
-    #return file_path
